b11b5ef5f43b6a6284cf826f34cbce7bb300751c
Atanu Bhowmick <mail2atanu007@gmail.com>
2019-08-24 17:27:17 +0530

    First commmit

---
+++ src/main/java/com/atanu/java/spring/offersvc/OfferSvcBoot/OfferSvcBootApplication.java
@@ -0,0 +1,13 @@
+package com.atanu.java.spring.offersvc.OfferSvcBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferSvcBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferSvcBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atanu/java/spring/offersvc/OfferSvcBoot/OfferSvcBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atanu.java.spring.offersvc.OfferSvcBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferSvcBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
