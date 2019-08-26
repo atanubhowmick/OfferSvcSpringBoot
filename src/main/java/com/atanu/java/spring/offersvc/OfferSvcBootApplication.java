f75efa5d50b985cc2e9e823dee1af2b6d3315309
Atanu Bhowmick <mail2atanu007@gmail.com>
2019-08-27 00:22:52 +0530

    Eureka client added

--- src/main/java/com/atanu/java/spring/offersvc/OfferSvcBootApplication.java
+++ src/main/java/com/atanu/java/spring/offersvc/OfferSvcBootApplication.java
@@ -2,11 +2,13 @@ package com.atanu.java.spring.offersvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Atanu Bhowmick
  *
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class OfferSvcBootApplication {
 
